504ff20fbf82a5b49d2f7ce9ec12b7c52ab7929b
DALGA Ozguc <ozgucdalga@gmail.com>
2021-11-15 22:06:53 -0500

    redux and provider were installed

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+/* acilan reducerlari index sayfasinda izleyebilmek icin provider kullanacagiz */
+import {Provider} from 'react-redux' 
+
+
+/* they are the packages about redux */
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
+
 import './bootstrap.min.css'
+
+import memoriesReducer from './reducers/memoriesReducer.js'
+
+const reducer =  combineReducers({
+  memories:memoriesReducer
+})
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider  store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
