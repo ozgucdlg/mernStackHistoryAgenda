d7d2eb41d0c7bdd7254a38e1e1ee8c476faa68eb
DALGA Ozguc <ozgucdalga@gmail.com>
2022-02-13 14:26:28 -0500

    Auth screen page

--- client/src/screens/AuthScreen.js
+++ client/src/screens/AuthScreen.js
@@ -3,7 +3,7 @@ import {Container,Row,Col,Form,Button} from 'react-bootstrap'
 
 const AuthScreen = () => {
 
-    const[login,setLogin] =useState(true);
+    const[login, setLogin] = useState(true);
 
 
   return (
@@ -16,7 +16,30 @@ const AuthScreen = () => {
             login  ? 
             
             <Form className="allign-content-center mt-3">
-                <h1>SIGN IN PLEASE</h1>
+                <h1  className="text-center mb-3">SIGN IN PLEASE</h1>
+
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                    TYPE='email'
+                    placeholder='type your email'></Form.Control>                   
+                    
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                    TYPE='password'
+                    placeholder='type your password'></Form.Control>                   
+                    
+                </Form.Group>
+                <Button block type='submit'>Sign in</Button>
+
+                <Form.text as='large' className='text-center mt-2'>Do you not have an account yet {' '} <span 
+                onClick={(e)=> setLogin(!login)}
+                style={{fontWeight:'bold',  cursor:'pointer'}}>
+                     </span>
+                     Create an account</Form.text>
             
             
             </Form>
